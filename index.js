840d2361f54da8a863ce7c9ded8e61872ca1bbb4
Gustavo Lisboa <gustavolisboadeveloper@gmail.com>
2023-07-17 12:21:45 -0300

    modifications

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-alert("EM CONSTRUÇÃO....")
+// alert("EM CONSTRUÇÃO....")
+//MANIPULAÇÃO DO DOM 
+const modal = document.querySelector('dialog')
+const comeceAqui = document.getElementById('comeceAqui')
+const btnClose = document.querySelector('.btnClose')
+
+
+//EVENTOS DO ESCULTA
+comeceAqui.addEventListener('onclick',function(){
+        modal.show()
+})
+btnClose.addEventListener("onclick",function(){
+       modal.close()
+})
